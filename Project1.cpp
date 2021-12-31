36b11ce1886f3fafe08cf7259071fe0135d5d58d
Franciscomaugusto <72284885+Franciscomaugusto@users.noreply.github.com>
2021-12-31 18:36:49 +0000

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -9,17 +9,6 @@
  */
 #include <iostream>
 #include <vector>
-/**
- * 
- * ASA 2021/2022 - IST
- * Projeto 1
- * 
- * 99218 - Francisco Augusto
- * 95908 - Rita Costa
- * 
- */
-#include <iostream>
-#include <vector>
 #include <cstring>
 #include <sstream>
 
@@ -58,13 +47,15 @@ vector<int> find_vector(string s,vector<vector<int>> v){
  * @param v 
  * @return int 
  */
-void alg_1(vector<vector<int>> v){
+vector<int> alg_1(vector<vector<int>> v){
     int max_rela = 0, max_absol, varia;
     vector<int> h= v[0];
+    vector<int> _results = vector<int> (2);
     int size = h.size();
     vector<vector<int>> _matriz (size, vector<int>(3, 0));
     if(size == 0){
-        printf("0 0");
+        _results[0] = 0;
+        _results[1] = 0;
     }
     else{
         for(int i= 0; i< size; i++){
@@ -72,7 +63,7 @@ void alg_1(vector<vector<int>> v){
         }
         for(int i= 0; i< size; i++){
             max_rela = 0;
-            varia = 0;
+            varia = 1;
             for(int h = 0; h < i; h++){
                 if(_matriz[i][0] > _matriz[h][0]){
                     if(_matriz[h][1] > max_rela){
@@ -83,12 +74,15 @@ void alg_1(vector<vector<int>> v){
                         varia++;
                     }
                 }
+                if(_matriz[i][0] == _matriz[h][0]){
+                    varia++;
+                }
             }
             _matriz[i][1] = max_rela + 1;
             _matriz[i][2] = varia;
         }
         max_absol = 0;
-        varia = 0;
+        varia = 1;
         for(int i = 0; i <size; i++){
             if(_matriz[i][1] >= max_absol){
                 max_absol = _matriz[i][1];
@@ -97,8 +91,10 @@ void alg_1(vector<vector<int>> v){
                 }
             }
         }
-        printf("%d %d\n",max_absol, varia);
+        _results[0] = max_absol;
+        _results[1] = varia;
     }
+    return _results;
 }
 /**
  * @brief 
@@ -106,32 +102,29 @@ void alg_1(vector<vector<int>> v){
  * @param v 
  */
 void alg_2(vector <int> v1, vector<int> v2){
-    int s1 = v1.size();
-    int s2 = v2.size();
-    int maior = 0;
-    int t[s1][s2];
-    for(int i = 0; i <s1; i++){
-        if(v1[i] == v2[0]){
-                t[i][0] = 1;
-            }
-        else{
-            t[i][0] = t[i-1][0];
-        }
+    long s1 = v1.size();
+    long s2 = v2.size();
+    vector<vector<int>> t (s1, vector<int>(s2));
+    if(v1[0] == v2[0]){
+        t[0][0] = 1;
     }
-    for(int j = 0; j< s2; j++){
+    else{
+        t[0][0] = 0;
+    }
+    for(long i = 1; i <s1/10; i++){
+            t[i][0] = 1;
+    }
+    for(long  j = 1; j< s2; j++){
         if(v1[0] == v2[j]){
                 t[0][j] = 1;
             }
-        else{
-            t[0][j] = t[0][j-1];
-        }
+
     }
 
-    for (int i = 1; i<s1 ; i ++){
-        for (int j = 1; j<s2 ; j ++){
+    for (long  i = 1; i<s1 ; i ++){
+        for (long   j = 1; j<s2 ; j ++){
             if (v1[i] == v2[j] && i != 0 && j != 0){
                 t[i][j] = t[i-1][j-1] + 1;
-                maior = t[i][j];
             }
             else{
                     t[i][j] = max(t[i-1][j], t[i][j-1]);
@@ -139,7 +132,7 @@ void alg_2(vector <int> v1, vector<int> v2){
         }
     }
 
-    printf("%d\n", maior);
+    printf("%d\n",t[s1-1][s2-1]);
 
 }
 
@@ -160,7 +153,8 @@ int main(){
     if(num == 1){
         getline(cin, line);
         _sequences[0] = find_vector(line,_sequences);
-        alg_1(_sequences);
+        vector<int> _results = alg_1(_sequences);
+        printf("%d %d\n", _results[0], _results[1]);
     }
     if(num == 2){
         getline(cin, line);
